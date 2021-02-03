c5a5eca3f811da3f72dc4823f9000cc4e38abb99
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-03 10:00:56 -0600

    feat: Finished the JavaScript and HTML exercise

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -13,7 +13,9 @@ console.log("confirmLeave " + confirmLeave);
 
 //Prompt
 var favoriteColor = prompt( "What is your favorite color?");
+
 console.log("Your favorite color is " + favoriteColor);
+
 alert("Great! " + favoriteColor + " is also my favorite color!" );
 
 var lilMerm = parseInt(prompt("How many days did you rent Little Mermaid?"));
@@ -27,28 +29,52 @@ var totalOwed = (lilMerm + brotherB + hercules)
 alert("Your total price for accumulated days is $" + totalOwed);
 
 var googleRate = prompt("What is your hourly rate working for Google?");
+
 var googleHours = prompt("How many hours did you work in total for Google?");
+
 var googlePay = (googleRate) * (googleHours);
+
 alert("This is how much you made working for Google: $" + (googlePay));
 
+
 var amazonRate = prompt("What is your hourly rate working for Amazon?");
+
 var amazonHours = prompt("How many hours did you work in total for Amazon?");
+
 var amazonPay = (amazonRate) * (amazonHours);
+
 alert("This is how much you made working for Amazon: $" + (amazonPay));
 
 var facebookRate = prompt("What is your hourly rate working for Google?");
+
 var facebookHours = prompt("How many hours did you work in total for Google?");
+
 var facebookPay =  (facebookRate) * (facebookHours);
+
 alert("This is how much you made working for Facebook: $" + (facebookPay));
 
 var totalPay = (Number(googlePay) + Number(amazonPay) + Number(facebookPay));
+
 alert("This is your total pay from the 3 jobs: $" + totalPay);
 
 var ClassSize = confirm("Are they any spots open in the class?");
+
 var ClassConflict = confirm("Are there no schedule conflicts?");
 
 alert("Are you able to enroll? " + (ClassSize && ClassConflict));
 
-var product = confirm("Are you buying more than two items?");
-var premium = confirm("Are you a premium member?");
-alert("Do you qualify for the discount?" + (product || premium));
+var hasMoreThanTwoItems = confirm("Do you have more than two items in your cart?");
+
+var isOfferValid = confirm("Is the offer still valid?");
+
+var isPremiumMember = confirm("Are you a premium member?");
+
+var canUserUseOffer = (hasMoreThanTwoItems || isPremiumMember) && isOfferValid;
+
+alert("You can use this offer is a " + canUserUseOffer + " statement.");
+
+//You can also do this.
+
+//var hasEnoughItems = (hasMoreThanTwoItems || isPremiumMember) && isOfferValid;
+//
+//var canUseOffer = hasEnoughItems && isOfferValid;
