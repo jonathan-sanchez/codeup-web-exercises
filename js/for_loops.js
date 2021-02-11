864baf038993db34c3f86502a39e722894a6d4f3
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-11 17:05:31 -0600

    feat: updated for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,51 @@
 "use strict"
 //For-loops exercise:
 
-function showMultiplicationTable(num){
-    for
+// for (var i = 0; i < 50; i++){
+// 	if(i === 25){
+// 		continue;
+// 	} else if (i % 6 === 0){
+// 		console.log("Codeup")
+// 	} else if (i % 2 === 0){
+// 		console.log("Code")
+// 	} else if (i % 3 === 0) {
+// 		{
+// 			console.log("Up")
+// 	}
+// 		console.log(i);
+// 	}
+// }
+function showMultiplicationTable(num) {
+    for (var i = 1; i <= 10; i++ ) {
+        var result = num * i;
+        console.log(num + " x " + i + " = " + result);
+    }
+
 }
+showMultiplicationTable(7)
+// function sayHelloXTimes(num) {
+//     for (num; num >= 0; num--) {
+//         if (num % 5 === 0) {
+//             console.log("Howdy! Currently num is: " + num);
+//         } else {
+//             console.log("Hello! Currently num is: " + num);
+//         }
+//     }
+//     // return "Hello!";
+// }
+
+// sayHelloXTimes(60)
 
 
 
+// for (var i = 0; i <= 10; i++) {
+//     var result = num * i;
+//     console.log(num + ' * ' + i + ' = ' + result);
+// }
+//
+//
+// for (var multiplier = 0; multiplier <= 10; multiplier++) {
+//     for (var i = 0; i <= 10; i++) {
+//         var result = multiplier * i;
+//         console.log(multiplier + ' * ' + i + ' = ' + result);
+//     }
