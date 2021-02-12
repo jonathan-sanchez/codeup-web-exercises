e5957e3f4cc198effe847413ebb849416053cc4b
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-12 16:39:50 -0600

    feat: Completed for_loops.js exercise.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,20 +1,37 @@
 "use strict"
-//For-loops exercise:
 
-// for (var i = 0; i < 50; i++){
-// 	if(i === 25){
-// 		continue;
-// 	} else if (i % 6 === 0){
-// 		console.log("Codeup")
-// 	} else if (i % 2 === 0){
-// 		console.log("Code")
-// 	} else if (i % 3 === 0) {
-// 		{
-// 			console.log("Up")
-// 	}
-// 		console.log(i);
-// 	}
-// }
+/*
+For-loops exercise:
+ for (var i = 0; i < 50; i++){
+ 	if(i === 25){
+ 		continue;
+ 	} else if (i % 6 === 0){
+ 		console.log("Codeup")
+ 	} else if (i % 2 === 0){
+ 		console.log("Code")
+ 	} else if (i % 3 === 0) {
+ 		{
+ 			console.log("Up")
+ 	}
+ 		console.log(i);
+ 	}
+ }
+Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+ For example, showMultiplicationTable(7) should output
+
+ 7 x 1 = 7
+ 7 x 2 = 14
+ 7 x 3 = 21
+ 7 x 4 = 28
+ 7 x 5 = 35
+ 7 x 6 = 42
+ 7 x 7 = 49
+ 7 x 8 = 56
+ 7 x 9 = 63
+ 7 x 10 = 70
+*/
+
 function showMultiplicationTable(num) {
     for (var i = 1; i <= 10; i++ ) {
         var result = num * i;
@@ -23,6 +40,7 @@ function showMultiplicationTable(num) {
 
 }
 showMultiplicationTable(7);
+
 // function sayHelloXTimes(num) {
 //     for (num; num >= 0; num--) {
 //         if (num % 5 === 0) {
@@ -33,15 +51,8 @@ showMultiplicationTable(7);
 //     }
 //     // return "Hello!";
 // }
-
+//
 // sayHelloXTimes(60)
-
-
-
-// for (var i = 0; i <= 10; i++) {
-//     var result = num * i;
-//     console.log(num + ' * ' + i + ' = ' + result);
-// }
 //
 //
 // for (var multiplier = 0; multiplier <= 10; multiplier++) {
@@ -49,27 +60,35 @@ showMultiplicationTable(7);
 //         var result = multiplier * i;
 //         console.log(multiplier + ' * ' + i + ' = ' + result);
 //     }
-for (var i = 0; i <= 10 ; i++){
-    var random = Math.floor((Math.random() * 200) + 20);
-    if (random % 2 === 0){
-        console.log(random + " is even");
+//Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+//
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+
+for (var i = 0; i <= 10; i++){
+    var randomNum = Math.floor((Math.random() * 200) + 20);
+    if (randomNum % 2 === 0){
+        console.log(randomNum + " is even");
     }else{
-        console.log(random + " is odd");
+        console.log(randomNum + " is odd");
     }
 }
 
-// for (var i = 1; i < 10; ++i){
-//     var
-//     if(i === 1 ){
-//         console.log();
-//     }
-// }
+//Create a for loop that uses console.log to create the output shown.
+var str = '';
 
-// for (var i = 100; i >= 5; i = i - 5) {
-//     var userNum = prompt("Please give me an odd number!");
-//     if (userNum % 3 === 0) {
-//         console.log(userNum + " is an odd number!");
-//     }else{
-//         console.log(userNum + " is not an odd number!")
-//     }
-// }
+for(var x = 0; x <= 9 ; x++) {
+    for(var y = 1; y <= x; y++) {
+        str += x;
+    }
+    str += "\n";
+}
+console.log(str);
+
+//Create a for loop that uses console.log to create the output shown.
+
+for (var i = 100; i >= 5; i = i - 5) {
+    console.log(i);
+}
