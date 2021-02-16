5d463de11e43d51f76b2913f691ab52429ab533d
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-15 18:12:43 -0600

    feat: add practice_js

---
+++ js/my_js_practice.js
@@ -0,0 +1,23 @@
+"use strict"
+console.log("What up Jon!");
+//Write a JavaScript program that accepts two integers and display the larger.
+// var myNum1, myNum2;
+// myNum1 = window.prompt("Hi! please give me a number!", "0");
+// myNum2 = window.prompt("Hey again! Please give me another number!", "0");
+//
+// if(parseInt(myNum1, 10) > parseInt(myNum2, 10))
+// {
+//     console.log("The larger of "+ myNum1+ " and "+ myNum2+ " is "+ myNum1+ ".");
+// }
+// else
+// if(parseInt(myNum2, 10) > parseInt(myNum1, 10))
+// {
+//     console.log("The larger of "+ myNum1+" and "+ myNum2+ " is "+ myNum2+ ".");
+// }
+// else
+// {
+//     console.log("The values "+ myNum1+ " and "+ myNum2+ " are equal.");
+// }
+//Write a JavaScript function that reverses a number.
+//example: x = 32243;
+//expected output: 34223.
