7ccf4fa1d3a3c84e4693679edfe43934d9755558
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-17 02:01:46 -0600

    feat: add my own js practice

--- js/my_js_practice.js
+++ js/my_js_practice.js
@@ -21,3 +21,47 @@ console.log("What up Jon!");
 //Write a JavaScript function that reverses a number.
 //example: x = 32243;
 //expected output: 34223.
+//
+// function reverseNum (number) {
+//     number = number + "";
+//     return number.split("").reverse().join("");
+// }
+// console.log(reverseNum(102811));
+//
+//The Fortune Teller
+//write a function named tellFortune:
+// function tellFortune (numChildren, partnersName, geoLocation, jobTitle){
+//     var future = "You will be a " + jobTitle + " in " + geoLocation + " and married to " + partnersName + " with " + numChildren + " kids."
+//     console.log(future)
+// }
+// tellFortune(2,"Hannah","Colorado", "Web Developer");
+//
+//The puppy age calculator:
+//Write a function named calculateDogAge:
+//
+// function calculateDogAge (pupAge) {
+//     var dogYears = 7 * pupAge;
+//     console.log("Your pup is " + dogYears + " years old in dog years!");
+// }
+// calculateDogAge(4);
+//
+//The lifetime Supply Calculator:
+//Write a function named calculateSupply:
+//
+// function calculateSupply (age, amountPerDay) {
+//     var maxAge = 100;
+//     var totalNeeded = (amountPerDay * 365) * (maxAge - age);
+//     var message = "You will need " + totalNeeded + " of twinkies to last you until the stinky old age of " + maxAge + ".";
+//     console.log(message);
+// }
+// calculateSupply(27, 1);
+
+var x = 0;
+
+while(x<6) {
+
+    x++;
+
+}
+
+document.write(x);
