ddc6726e3dc54d5e4723ed849d0a34884a8b08da
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-19 02:35:59 -0600

    feat: add updates to break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,17 @@
 "use strict"
 //Break and Continue exercise:
-for (i = 1; i < 50; i++) {
-    var x = prompt("Please give me an odd number between 1 and 50.");
-   console.log()
+//
+// for (var i = 1; i < 50; i++) {
+//     if (i % 1 === 0) {
+//         var num = prompt("Give me an odd number between 1 and 50!");
+//         console.log("The number to skip is: " + num);
+//     }else if (i % 2 === 0){
+//         console.log(prompt("That is not an odd number! Try again!"));
+//     }
+// }
+var i = 0;
+do {
+    console.log(i);
+    i++
 }
+while (i < 50);
