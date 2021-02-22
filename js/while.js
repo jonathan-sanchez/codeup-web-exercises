75fd5444e8be9e3b2676540e0bed97adf7624ae0
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-02-22 15:51:04 -0600

    feat: add js/while.js

---
+++ js/while.js
@@ -0,0 +1,33 @@
+"use strict"
+//While loops Exercise:
+//Lecture Example:
+// var number = 2;
+//
+// while (number <= 65536) {
+//     console.log(number);
+//     number = number * 2;
+//     // number *= 2;
+// }
+//My Answer:
+
+//Do-While Loop Exercise:
+//Lecture Example:
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// console.log("You have " + allCones + " cones to sell today!");
+// do {
+//     var requestedCones = Math.floor(Math.random() * 5) + 1;
+//
+//     if (requestedCones <= allCones) {
+//         //This means you can sell the cones.
+//         allCones = allCones - requestedCones;
+//         console.log("You sold " + requestedCones + ", and have " + allCones + " cones left.")
+//
+//     }else{
+//         //This means you dont have enough cones to sell.
+//         console.log("Sorry i dont have " + requestedCones + " cones, I only have " + allCones + " cones left.");
+//     }
+//
+// }while (allCones > 0)
+//
+// console.log("Woohoo! Sold all of the cones for the day!");
+//My Answer:
