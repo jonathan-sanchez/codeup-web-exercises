40b2de60d6c1ac387c2a62818e02ea4a102820d0
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-03-29 11:58:53 -0500

    feat: added dom-query-js.html

--- js/DOM-student.js
+++ js/DOM-student.js
@@ -1,28 +1,47 @@
 //i. locating in the DOM
 
 // TODO: Find an element by the id of "hat-search". Store the result into a variable. Use a console log to check what is in your variable.
-    var getElementById = document.getElementById("hat-search");
 
-    console.log(getElementById);
+    var hatSearch = document.getElementById("hat-search");
+
+    console.log(hatSearch);
+
 // TODO: Find all of our elements we made flex containers in Bootstrap! Locate the .d-flex class, store all of your results into a variable, and then console.log them.
-    var getElementByClassName= document.getElementsByClassName(".d-flex");
 
-    console.log(getElementByClassName);
+    var flexContainers = document.getElementsByClassName(".d-flex");
+
+    console.log(flexContainers);
+
 // TODO: Finish my FOR loop to go through that variable containing our d-flex'ed items and console.log each individual one within the loop!
 
-for(var ; i < .length; ++){
-    length;
-}
+    for(var i = 0; i < flexContainers.length; i++){
+    console.log("what is i: " + i);
+
+    console.log(flexContainers[i]);
+    }
 
 
 // TODO: Find all of our div elements and store them into a variable~! Console.log the variable to see the results within!
 
+    var divElements = document.getElementsByTagName("div");
+
+    console.log(divElements);
+
 // TODO: Now that we have a variable with all of the elements, can you tell me how many divs are in the HTML using that variable and a little Javascript? Hint: the variable looks and behaves a lot like an array, doesn't it? That helped us in our for loop's condition!
 
+    console.log(divElements.length);
 
 // TODO BONUS (a): Write a function called locateDivs. When locateDivs is called, it should return a collection of the div elements found.
 
+    function locateDivs() {
+        return document.getElementsByTagName("div");
+    }
+
 // TODO BONUS (b): Write a function called locateElements. locateElements should take in one parameter, the element to be searched for, and then return a collection of those elements. If no elements by that name were found, return false.
+
+    function locateElements(element) {
+        return document.getElementsByTagName(element);
+    }
 
 // ii. a step further: accessing / looking into what we've located deeper
 
