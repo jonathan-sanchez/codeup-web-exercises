eee4a09464c5ed1c39024b3c45e9597cd37486cc
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-04-06 14:43:52 -0500

    added jquery keyboard events lecture

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,12 @@
 'use strict'
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+// var contents = $('#special-item').html();
+// alert(contents);
+
+$('codeup').css('border', '1px solid red');
+
+;
