7d190cab73b358add01f0e3aa9082f217fc79dd8
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-04-08 09:16:03 -0500

    feat: updated essentialMethods

--- js/essentialMethods-live.js
+++ js/essentialMethods-live.js
@@ -5,30 +5,54 @@
 
 //TODO: Let's get the HTML from the element with the ID of "best-seller-cont"
 
+console.log($("#best-seller-cont").html()); //Get the html at this id (#).
+
 //TODO together: Let's set up a string with HTML to replace this old list with our updated best sellers - FEDORAS have moved into the number one spot!
+
+var html = "";
+
+html = "<h4 class=\"best-seller-header\">Our Top Sellers</h4><ol><li class='hat-sold'>Fedora Hats</li><li class='hat-sold'>Baseball Hats</li><li class='hat-sold'>Cowboy Hats</li></ol>"
 
 //TODO: Now, let's get back into that "best-seller-cont" and replace that HTML!
 
+$("#best-seller-cont").html(html); //SETTING some HTML
+
 //TODO Together: Can we make a click event that changes "Howell's" to "Marco's" when we click on it? Help me out!
+
+$(".navbar-brand").click(function(){
+    $(this).html("Marco's <i class=\"bi-shop mx-1\"></i>");
+});
 
 //.css() - get info about a property or change a property
 
 //TODO: Let's see what the width of our "main-content" class is with .css()
 
+console.log($(".main-content").css("width")); //Getting some information. (note: one argument)
+
 //TODO Together: Can you help me? I'd like to set my modal's text to have a cool color :)
+
+$(".modal-body").css("color", "red"); //setting some CSS (note: two arguments - a. our property and b. our value.
 
 //TODO: Let's make an object for us to use with .css(). .
 
+var warningRed = {"background-color": "cornflowerblue", "font-size": "72px"};
+
 //TODO Together: Nice - now, let's test this out by targeting our h1 with this warning background! Next, let's chain a font color after it
+$("h1").css(warningRed).css("color", "rebeccapurple");
 
 //.addClass() ~ .removeClass() ~ .toggleClass()
 
 //TODO: We've used a lot of bootstrap on the page - let's see what we can use .addClass to do in combination with bootstrap. Let's UPPERCASE that h1 with the shiny red background!
-
+$("h1").addClass("text-uppercase");
 //TODO Together: Can you assist me in removing the "bg-dark" background from our FOOTER?
 
+$("footer").removeClass("bg-dark");
 //TODO: I have a custom css class known as hi-lite - .toggleClass() with a .click() would be a neat way to use it
-
+$("*").click(function(e){
+    e.preventDefault();
+    var ele = $(e.target);
+    ele.toggleClass("hi-lite");
+});
 
 //Bonus activity:
 
@@ -47,20 +71,32 @@
 //traversing
 
 //.each() - iterate over each matched element to do some stuff
-//TODO: Let's change the last two lis of the class .hat-sold to "Baseball hats" as well
+//TODO: Let's change the last two lis of the class .hat-sold to "Fedora hats" as well
+
+$(".hat-sold").each(function(index, ele){ //Iterate over matched elements.
+    if (index !== 0) {
+        $(ele).html("Fedora hats"); //Set HTML to value
+    }
+})
 
 //.first()
-//TODO: Let's change the first list item in the class of .hat-sold from "Baseball hats" to "Porkpie hats"
+//TODO: Let's change the first list item in the class of .hat-sold from "Fedora hats" to "Porkpie hats"
+
+$(".hat-sold").first().html("Porkpie hats");
 
 //.last()
 //TODO Together: Let's change the last li in the class of .hat-sold to be Fancy hats - italicized! ! !
 
+$(".hat-sold").last().html("<em>Fancy hats</em>"); //OOPS. Did  we just method chain? We sure did!
+
 //.parent()
 //TODO: Let's pop up the tree to the parent (<ol>) and use .css to change the list-style-type
 
+$(".hat-sold").parent("ol").css("list-style-type", "upper-roman"); //Another method chain!
+
 //.children()
 //TODO Together: What ID would I need to target to see all of the HTML, as children, that we've been manipulating so far?
-
+console.log($("#best-seller-cont").children());
 //.next()
 //TODO: Let's slide across branches (sibling to sibling) on our tree and change the HTML within the ordered list we find there to only show Top Hats! !
-
+console.log($(".best-seller-header").next().html("<li>Top Hat</li>"));
